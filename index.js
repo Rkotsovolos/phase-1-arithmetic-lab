88e1554a56fecaff94474ac2e721defa57da000b
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-11 15:19:48 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+const num1 = 62;
+const num2 = 1;
+let multiply = num1 * num2;
+
+multiply;
+
+let random = Math.ceil(Math.random());
+
+random;
+
+
+
+const num3 = 16;
+const num4 = 4;
+let mod = num3 / num4;
+mod;
+
+
+let max = Math.max(1,2,3,4,5,6,7,20);
+max;
